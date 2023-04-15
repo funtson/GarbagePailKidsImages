524bbb936d59790788cdf990712b26386f44931e
funtson <john@doe.org>
2023-04-15 11:12:16 -0400

    Rotate landscape images

--- Themed Series/Battle of the Bands/scraper.py
+++ Themed Series/Battle of the Bands/scraper.py
@@ -4,11 +4,12 @@ import os
 # define the base URL for the images
 base_url = "https://www.youknowthewebsite.com/gallery/"
 # define the total number of stickers in each series
-num_stickers = [20 , 40, 20, 20, 42, 20]
-prefixes = ["alternative", "classicrock", "metal", "newwave", "pop", "rap"]
+num_stickers = [20 , 40, 20, 20, 42, 20, 9]
+prefixes = ["alternative", "classicrock", "metal", "newwave", "pop", "rap", "hardrock"]
+
 counter = 1
 # iterate through each series
-for i in range(1, 7):
+for i in range(1, len(num_stickers)+1):
     prefix = "bands"
     # generate the series folder name and file name prefix
     series_folder = prefix
@@ -17,18 +18,11 @@ for i in range(1, 7):
     # iterate through each sticker in the series
     for j in range(1, num_stickers[i-1]+1):
         # generate the URLs for the "a" and "b" stickers
-        url_a = base_url + series_folder + "/" + file_prefix + str(j) + "a.jpg"
-        url_b = base_url + series_folder + "/" + file_prefix + str(j) + "b.jpg"
+        url_a = base_url + series_folder + "/" + file_prefix + str("{:03d}".format(j)) + ".jpg"
          
         # make a GET request for the "a" sticker and save the image
-        if not os.path.exists(file_prefix + str(j) + "a.jpg"):
+        if not os.path.exists(file_prefix + str(j) + ".jpg"):
             response_a = requests.get(url_a)
-            with open(file_prefix + str(j) + "a.jpg", "wb") as f:
+            with open(file_prefix + str(j) + ".jpg", "wb") as f:
                 f.write(response_a.content)
-    
-        # make a GET request for the "b" sticker and save the image
-        if not os.path.exists(file_prefix + str(j) + "b.jpg"):
-            response_b = requests.get(url_b)
-            with open(file_prefix + str(j) + "b.jpg", "wb") as f:
-                f.write(response_b.content)
         counter += 1
